b9b0460c2fef5ea3fc74f1b8fb03e34f0b0c4370
Alexander Gribkov <gribkovalexander@gmail.com>
2024-04-03 14:58:15 +0300

    Home task 10: v1 again -_-

--- myworks/hometask/task10/v1.py
+++ myworks/hometask/task10/v1.py
@@ -1,41 +1,51 @@
-def main(table_in):
-    table = table_in.copy()
-    table = remove_duplicates(table)
-    table = list(filter(
-        lambda line: not all(map(lambda element: element == "None", line)),
-        table
-    ))
-    table = change_elements(table)
-    table.sort(key=lambda line: line[2])
-    return [
-        [
-            table[j][i]
-            for j in range(len(table))
-        ]
-        for i in range(len(table[0]))
-    ]
-
-
-def remove_duplicates(table):
-    return list(map(
-        lambda line: line.split("\n"),
-        list(dict.fromkeys(
-            map(lambda line: "\n".join(
-                list(
-                    map(
-                        lambda element:
-                        "None" if element is None
-                        else element,
-                        line
-                    )
-                )
-            ), table)
-        ))))
-
-
-def change_elements(table):
+def get_sorted_list(table_in):
+    table = sorted(
+        table_in,
+        key=lambda line: "`" if line[0] is None else line[2]
+    )
+    table = list(filter(lambda line: line[0] is not None, table))
+    res = []
     for line in table:
-        line[0] = line[0].split("@")[0]
-        line[1] = f"{float(line[1]):.3f}"
-        line[2] = line[2][:13] + "-" + line[2][13:]
+        if line not in res:
+            res.append(line)
+    return res
+
+
+def first_column(table_in):
+    table = []
+    table_temp = []
+    for i in range(len(table_in)):
+        table_temp.append(table_in[i][0])
+        table.append(table_in[i][0].split("@")[0])
     return table
+
+
+def second_column(table_in):
+    table = []
+    table_temp = []
+    for i in range(len(table_in)):
+        table_temp.append(table_in[i][1])
+        table.append(table_in[i][1] + "00")
+    return table
+
+
+def third_column(table_in):
+    table = []
+    table_temp = []
+    for i in range(len(table_in)):
+        table_temp.append(table_in[i][2])
+        table.append(
+            table_in[i][2][:13] +
+            "-" +
+            table_in[i][2][13:]
+        )
+    return table
+
+
+def main(table_in):
+    table_in_sorted = get_sorted_list(table_in)
+    return [
+        first_column(table_in_sorted),
+        second_column(table_in_sorted),
+        third_column(table_in_sorted)
+    ]
